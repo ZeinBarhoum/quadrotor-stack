a1db9ae403622977d1b88c0559df59293ac07da4
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2023-07-14 13:30:00 +0300

    Add planning-only launch configuration

---
+++ src/quadrotor_bringup/launch/quadrotor_planning.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    reference_publisher_node = Node(
+        package= 'quadrotor_trajectory_generation',
+        executable= 'quadrotor_reference_publisher',
+        output= 'screen'
+    )
+
+    trajectory_poly_optimizer_node = Node(
+        package = 'quadrotor_trajectory_generation',
+        executable = 'quadrotor_poly_optimizer',
+        output = 'screen'
+    )
+
+    mapping_node = Node(
+        package = 'quadrotor_mapping',
+        executable = 'quadrotor_default_map',
+        output = 'screen'
+    )
+    path_finding_node = Node(
+        package = 'quadrotor_path_finding',
+        executable= 'quadrotor_rrt',
+        output = 'screen'
+    )
+
+    return LaunchDescription([
+        reference_publisher_node,
+        trajectory_poly_optimizer_node,
+        mapping_node,
+        path_finding_node,
+    ])
