4019533a26a6858af876036d0eab179ec2169536
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2023-07-20 10:20:30 +0300

    Add waypoints to the reference plot

--- src/quadrotor_dashboard/quadrotor_dashboard/quadrotor_path_visualizer.py
+++ src/quadrotor_dashboard/quadrotor_dashboard/quadrotor_path_visualizer.py
@@ -25,8 +25,7 @@ class QuadrotorPathVisualizer(Node):
         
         self.timer_plot = self.create_timer(timer_period_sec= 0.1, 
                                             callback= self.plot_callback)
-        # self.fig = plt.figure()
-        # self.ax3 = self.fig.add_subplot(111, projection='3d')
+
         plt.ion()
         
         self.fig, self.ax3 = plt.subplots(1, 1, subplot_kw={'projection': '3d'})
@@ -34,14 +33,15 @@ class QuadrotorPathVisualizer(Node):
         self.current_reference = [0,0,0]
 
     def waypoints_callback(self, msg):
-        # waypoints = np.array([[p.x, p.y, p.z] for p in msg.waypoints])
-        # self.ax3.scatter3D(waypoints[:,1], waypoints[:,2], waypoints[:,3])
-        # print(msg)
-        pass
+        x = [waypoint.x for waypoint in msg.waypoints]
+        y = [waypoint.y for waypoint in msg.waypoints]
+        z = [waypoint.z for waypoint in msg.waypoints]        
+        self.ax3.scatter(x, y, z, c = 'b', marker='o')
+        
     def plot_callback(self):
         
         self.ax3.plot(*self.references, c='r')
-        self.ax3.scatter(*self.current_reference, c='b', marker='o')
+        # self.ax3.scatter(*self.current_reference, c='b', marker='o')
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
         
@@ -53,14 +53,6 @@ class QuadrotorPathVisualizer(Node):
         
         self.current_reference = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
         
-        # self.ax3.scatter(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
-        # self.ax3.scatter(*self.references, c='r', marker='o')
-        # self.fig.canvas.draw()
-        # self.fig.canvas.flush_events()
-        # self.ax3.scatter(*self.references, c='r', marker='o')
-        # # self.ax3.scatter(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
-        # plt.pause(0.01)
-        # plt.show()
 
 
 def main():
