5246f1244d223ee73e626e41afb15d6f572ce641
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2023-09-22 20:15:37 +0300

    Add common map transformations and collision detection to utils

---
+++ src/quadrotor_utils/quadrotor_utils/collision_detection.py
@@ -0,0 +1,105 @@
+"""Common collision detection functions."""
+import numpy as np
+import numpy.typing as npt
+from quadrotor_interfaces.msg import PolynomialSegment, PolynomialTrajectory, OccupancyGrid3D
+from quadrotor_utils.map_transformations import OccupancyGrid3D_to_voxelmap, point_to_voxel
+
+
+def detect_collision_voxelmap(voxel_map: npt.ArrayLike, voxel: npt.ArrayLike) -> bool:
+    """Detects collision between a point and a 3D map
+
+    Args:
+        voxel_map (ArrayLike): 3D map of the environment
+        voxel (ArrayLike): 3D point to check for collision 
+
+    Raises:
+        ValueError: Map must be 3D
+        ValueError: Point must be a vector
+        ValueError: Point must be 3D
+
+    Returns:
+        bool: True if there is a collision, False otherwise
+    """
+    voxel_map = np.array(voxel_map)
+    voxel = np.array(voxel)
+
+    if voxel_map.ndim != 3:
+        raise ValueError('Map must be 3D')
+
+    if voxel_map.ndim != 1:
+        raise ValueError('Point must be a vector')
+
+    if voxel_map.shape[0] != 3:
+        raise ValueError('Point must be 3D')
+
+    for x in [np.floor(voxel[0]), np.ceil(voxel[0])]:
+        for y in [np.floor(voxel[1]), np.ceil(voxel[1])]:
+            for z in [np.floor(voxel[2]), np.ceil(voxel[2])]:
+                if map[int(x), int(y), int(z)] == 1:
+                    return True
+    return False
+
+
+def detect_collision_OccupancyGrid3D(grid: OccupancyGrid3D, point: npt.ArrayLike) -> bool:
+    """Detects collision between a point and a 3D map
+
+    Args:
+        grid (OccupancyGrid3D): 3D map of the environment
+        point (ArrayLike): 3D point to check for collision 
+
+    Returns:
+        bool: True if there is a collision, False otherwise
+    """
+    voxel = point_to_voxel(grid, point)
+    voxel_map = OccupancyGrid3D_to_voxelmap(grid)
+    return detect_collision_voxelmap(voxel_map, voxel)
+
+
+def detect_collision_trajectory_segment(grid: OccupancyGrid3D,
+                                        segment: PolynomialSegment,
+                                        precision: float = 0.1,
+                                        ):
+    """Detects collision between a trajectory segment and a 3D map
+
+    Args:
+        grid (OccupancyGrid3D): 3D map of the environment
+        segment (PolynomialSegment): Trajectory segment to check for collision
+        precision (float, optional): Precision of the trajectory segment in seconds. Defaults to 0.1.
+    Returns:
+        bool: True if there is a collision, False otherwise
+    """
+    poly_x = segment.poly_x
+    poly_y = segment.poly_y
+    poly_z = segment.poly_z
+    t0 = segment.start_time
+    tend = segment.end_time
+
+    test_times = np.arange(t0, tend, precision)
+    for t in test_times:
+        x = np.polyval(poly_x, t)
+        y = np.polyval(poly_y, t)
+        z = np.polyval(poly_z, t)
+        point = np.array([x, y, z])
+        if detect_collision_OccupancyGrid3D(grid, point):
+            return True
+    return False
+
+
+def detect_collision_trajectory(grid: OccupancyGrid3D,
+                                trajectory: PolynomialTrajectory,
+                                precision: float = 0.1,
+                                ):
+    """Detects collision between a trajectory and a 3D map
+
+    Args:
+        grid (OccupancyGrid3D): 3D map of the environment
+        trajectory (PolynomialTrajectory): Trajectory to check for collision
+        precision (float, optional): Precision of the trajectory segment in seconds. Defaults to 0.1.
+    Returns:
+        bool: True if there is a collision, False otherwise
+    """
+    segments = trajectory.segments
+    for segment in segments:
+        if detect_collision_trajectory_segment(grid, segment, precision):
+            return True
+    return False
