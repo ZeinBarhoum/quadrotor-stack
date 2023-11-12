dcd4db5cd69906b357c5542df48532be5755af5b
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2023-11-12 13:37:54 +0300

    tidy up some un-used files

--- src/quadrotor_bringup/launch/quadrotor_simulation.launch.py
+++ src/quadrotor_bringup/launch/quadrotor_simulation.launch.py
@@ -9,16 +9,6 @@ import os
 
 
 def generate_launch_description():
-
-    decalred_arguments = []
-
-    decalred_arguments.append(
-        DeclareLaunchArgument(
-            name='controller',
-            default_value='quadrotor_pid',
-            description='Controller to use (default: quadrotor_pid)'
-        )
-    )
     config_folder = os.path.join('src', 'quadrotor_bringup', 'config')
     config_file = os.path.join(config_folder, 'simulation.yaml')
     with open(config_file, "r") as stream:
@@ -27,26 +17,25 @@ def generate_launch_description():
         except yaml.YAMLError as exc:
             parameters = dict()
     path_visualizer_parameters = parameters['QuadrotorPathVisualizer']
-    pybullet_simulation_parameters = parameters['PybulletSimulation']
+    pybullet_physics_simulation_parameters = parameters['PybulletPhysicsSimulation']
+    pybullet_camera_simulation_parameters = parameters['PybulletCameraSimulation']
     pid_controller_parameters = parameters['PIDController']
     dfbc_controller_parameters = parameters['DFBCController']
     poly_traj_optimizer_parameters = parameters['PolyTrajOptimizer']
 
-    simulation_node = Node(
+    simulation_physics_node = Node(
         package='quadrotor_simulation',
-        executable='quadrotor_pybullet',
+        executable='quadrotor_pybullet_physics',
         output='screen',
-        parameters=[pybullet_simulation_parameters]
+        parameters=[pybullet_physics_simulation_parameters]
+    )
+    simulation_camera_node = Node(
+        package='quadrotor_simulation',
+        executable='quadrotor_pybullet_camera',
+        output='screen',
+        parameters=[pybullet_camera_simulation_parameters]
     )
 
-    controller = LaunchConfiguration("controller")
-
-    pid_controller_node = Node(
-        package='quadrotor_control',
-        executable=controller,
-        output='screen',
-        parameters=[pid_controller_parameters]
-    )
     dfbc_controller_node = Node(
         package='quadrotor_control',
         executable='quadrotor_dfbc',
@@ -84,21 +73,25 @@ def generate_launch_description():
             {'refresh_rate': path_visualizer_parameters['refresh_rate']}],
         output='screen'
     )
+    image_visualizer_node = Node(
+        package='quadrotor_dashboard',
+        executable='quadrotor_image_visualizer',
+        output='screen'
+    )
     rqt_gui_node = Node(
         package='rqt_gui',
         executable='rqt_gui',
         output='screen',
     )
 
-    return LaunchDescription(
-        decalred_arguments+[
-            simulation_node,
-            # pid_controller_node,
-            dfbc_controller_node,
-            reference_publisher_node,
-            trajectory_poly_optimizer_node,
-            mapping_node,
-            path_finding_node,
-            path_visualizer_node,
-            rqt_gui_node,
-        ])
+    return LaunchDescription([simulation_physics_node,
+                              simulation_camera_node,
+                              dfbc_controller_node,
+                              reference_publisher_node,
+                              trajectory_poly_optimizer_node,
+                              mapping_node,
+                              path_finding_node,
+                              path_visualizer_node,
+                              image_visualizer_node,
+                              rqt_gui_node,
+                              ])
